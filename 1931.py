79b457a8d6b583da95ac674e053cd53faa6d3d45
Minjae Baek <102715118+jayjayjayjayjayjayjayjayjay@users.noreply.github.com>
2022-09-06 00:41:47 +0900

    Add files via upload

---
+++ 1931.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline # 런타임 빨라서 씀
+
+n = int(input())
+
+list = []
+for i in range(n):
+  s,e = map(int,input().split())
+  list.append([s,e])
+
+list.sort(key = lambda x : x[0])
+list.sort(key = lambda x : x[1])
+
+cnt = 0
+k = 0
+for i in range(len(list)):
+  j = list[i][0]
+  if j >=k:
+    cnt+=1
+    k = list[i][1]
+print(cnt)
+
+
+
